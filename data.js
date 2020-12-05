24049892e598c1f7756d937bb34121e2fa8cacd2
Veekeey <meligavictoria@gmail.com>
2020-12-05 08:37:58 +0100

    commit for host

---
+++ data.js
@@ -0,0 +1,65 @@
+let allData = [
+  { 
+      id: "civic",
+      description: "An event center offering a breathtaking waterfront view from its three banqueting halls, Civic Center is one of the most beautiful and fascinating structures Lagos has to offer.Before heading to Civic Center, a swimming gear is one of the helpful things to take along with you. "
+  },
+     
+  {
+      id: "coconut",
+      description: "Coconut Beach is surrounded by holiday resorts where revelers could refresh while visiting the beach, with the kind of tropical sun that could be enjoyed within the environment. Coconut Beach is an ideal destination for lovers of fun and holidaymakers. Revelers are often left with options of enjoying the ocean breeze or watch the ocean as it rages and roils. Tall coconut trees form bewitching scenery. The sun seems to be resting on the edge of the horizon where sky and water meet, creating a kind of colourful contrast. This beach is known to have similar features of many beaches in Lagos State.The beach entertains various fun-seekers with music and other musical jamboree. It is an ideal destination for any fun-seekers during the holiday or yuletide season."
+  },
+
+  {
+      id: "giant",
+      description: "Located in a sacred grove between Ukhuse-oke and Ukhuseosi in lulehe clan of Owan west local government area,very distinct footprints of a prehistoric giant are ."
+
+  },
+  
+  {
+      id: "ibeno",
+      description: "It is the most popular beach in Akwa Ibom. The beach itself is a must see as it is the longest beach in Africa. a bridge which was built by the federal government.With its beautiful coastline, Ibeno provides endless natural facilities for tourist, such as;  Boat Ridin The beach is open to families on vacation, class gro"
+  },
+
+  {
+      id: "kainji",
+      description: "Located in the northwest central part of the country in Niger and Kwara states, Kainji Lake National Park is a typical inselberg landscape with round hills wide pediments/plains separating them and incised river channels slopping down towards the River Niger valley."
+  },
+
+  {
+      id: "kajuru",
+      description: "Kajuru Castle is an exclusive and luxurious total getaway built in an early medieval style at Kajuru village in the trade city of Kaduna, a major commercial hub in the northern part of Nigeria. This gorgeous architectural masterpiece with breathtaking ambience is set upon elevated peaks with staggering views. Kajuru Castle is styled in a European and clearly German way with a baronial hall, complete with suits of armour. This castle operates on an exclusive basis and can only accommodate 12 individuals or 5 couples at a time."
+  },
+
+  {
+      id: "kano",
+      description: "The ancient Kano city walls were built in order to provide security to the growing population. The foundation for the construction of the wall was laid by Sakri Gijimasu from 1095 - 1134 and was completed in the middle of 14th Century during the reign of Zamnagawa. In the 16th century, the walls were further extended to their present position. The gates are as old as the walls and were used to control movement of people in and out of the city. Though, most of them are largely in ruins."
+  },
+
+  {
+      id: "millenium",
+      description: "Picture this: A beautifully designed walkway, flanked on each side by glimmering green grass, leading down to a refreshing fountain, the evening breeze brushing you softly under the illuminating night-lights… Now come back to reality! Millennium Park is the name of this breath-taking arena. Whether you are looking to get in touch with Mother Nature, have some quality personal time, or take a walk with that special person, Millennium Park opens up the window for you to leap out of reality. "
+  },
+
+  {
+      id: "museum",
+      description: "The Nigerian National Museum is a national museum of Nigeria, located in the city of Lagos. The museum has a notable collection of Nigerian art, including pieces of statuary and carvings and archaeological and ethnographic exhibits. Of note is a terra-cotta human head known as the Jemaa Head (c. 900 to 200 BC), part of the Nok culture. The piece is named after Jema'a, the village where it was uncovered. It is located at Onikan, Lagos Island."
+  },
+
+  {
+      id: "obudu",
+      description: "The Obudu Mountain Resort has capitalized on its altitude to provide guests with the best view points and fresh, invigorating mountain air. A short walk takes you to the spectacular grotto. This natural swimming pool with sparkling pure water provides a clear view of the long winding waterfall and green mountain ranges surround the natural reserve. The reserve is home to rare species of birds and other animals such as the gorilla and drill monkey."
+  },
+
+  {
+      id: "silverbird",
+      description: "Silverbird Galleria was established in 2004 by Silverbird Group,[2] a media and real estate company founded by Ben Murray-Bruce in the 1980s. The movie theatre, Silverbird Cinemas which revolutionalized cinema in Nigeria[3] pioneered the first five-screens Cineplex in sub-Saharan Africa. Silverbird Cinemas also possesses the largest cinema chain in West Africa with several locations in Lagos, Abuja, Port Harcourt, Uyo and Accra, Ghana."
+  },
+
+  {
+      id: "yankari",
+      description: "Yankari Game Reserve is a large wildlife park located in the south-central part of Bauchi State, in northeastern Nigeria. It covers an area of about 2,244 square kilometres (866 sq mi) and is home to several natural warm water springs, as well as a wide variety of flora and fauna. Its location in the heartland of the West African savanna makes it a unique way for tourists and holidaymakers to watch wildlife in its natural habitat."
+  }
+
+
+]
+
---
+++ tour.js
@@ -0,0 +1,68 @@
+//accepts a description and renders a modal with it.
+function renderModal(content){
+    document.getElementById("bigmod").style.display = "block";
+    let btn = document.getElementById('book-btn');
+    btn.insertAdjacentHTML('beforebegin', content);
+    
+}
+
+
+
+//extracts the id from every individual IMG element
+function handleClick() {
+
+    document.onclick = function(e){
+
+        let currentId = e.target.getAttribute('id');
+      
+        for(let data in allData){
+            if(allData[data].id == currentId ){
+
+                renderModal(allData[data].description);
+            }
+        }     
+
+    }
+   
+}
+
+
+
+// function to close modal
+function closeModal() {
+  document.getElementById("bigmod").style.display = "none";
+}
+
+
+
+
+// booking form validation
+document.querySelector('.find-submit').addEventListener('click', (e) => {
+  e.preventDefault();
+
+  let userInput = document.querySelector('.fullname').value;
+  let userEmail = document.querySelector('.email').value;
+  let userPhone = document.querySelector('.phone-num').value;
+
+  if (userInput.length == 0) {
+    document.querySelector('.error').style.display = 'block';
+  }else {
+    document.querySelector('.error').style.display = 'none';
+  }
+
+  if (userPhone.length < 11 || userPhone.length > 11) {
+    document.querySelector('.phone-error').style.display = 'block';
+  }else {
+    document.querySelector('.phone-error').style.display = 'none';
+  }
+
+  let reg = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  let regResponse = reg.test(userEmail);
+    if (regResponse) {
+      document.querySelector('.email-error').style.display = 'none';
+    }else {
+      document.querySelector('.email-error').style.display = 'block'
+    }
+    
+})
+
